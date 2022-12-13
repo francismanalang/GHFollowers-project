ea83e25591522216ac7d5015c26c194a3b1f0f73
Francis Manalang <francismanalang7@gmail.com>
2022-12-12 21:49:06 -0800

    GFTitle Label created

---
+++ GHFollowers/GFTitleLabel.swift
@@ -0,0 +1,22 @@
+//
+//  GFTitleLabel.swift
+//  GHFollowers
+//
+//  Created by Francis Manalang on 12/12/22.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
